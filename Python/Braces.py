c74919e42996372f5d841be832d2f81d28bf946b
David McLaughlin <mclaughlin14@gmail.com>
2017-11-30 21:51:23 -0500

    fixed .gitignore file

--- Python/Braces.py
+++ Python/Braces.py
@@ -1,26 +1,27 @@
 def braces(values):
-  stack = []
-  pushChars = "({["
-  popChars = ")}]"
-  for char in values:
-    if char in pushChars:
-      stack.append(char)
-    elif char in popChars:
-      if not len(stack):
-        return "NO"
-      else:
-        stackTop = stack.pop()
-        balancingBracket = pushChars[popChars.index(char)]
-        if stackTop != balancingBracket:
-          return "NO"
-    else:
-      return "NO"
-  return "YES"
+    stack = []
+    pushChars = "({["
+    popChars = ")}]"
+    for char in values:
+        if char in pushChars:
+            stack.append(char)
+        elif char in popChars:
+            if not len(stack):
+                return "NO"
+            else:
+                stackTop = stack.pop()
+                balancingBracket = pushChars[popChars.index(char)]
+                print(balancingBracket)
+                if stackTop != balancingBracket:
+                    return "NO"
+        else:
+            return "NO"
+    return "YES"
 
 
 test_string = "()[{}]"
-test_string2 = "{}[[])"
+# test_string2 = "{}[[])"
 
 test = braces(test_string)
 print(test)
-print(braces(test_string2))
+# print(braces(test_string2))
--- Python/OddNumbers.py
+++ Python/OddNumbers.py
@@ -1,5 +1,11 @@
+# Write a function that takes 2 numbers
+# and returns a list of the odd numbers
+# between them, starting with the first
+# and ending with the last (inclusive).
+
+
 def oddNumbers(l, r):
-    listofnumbers = list(range(1, r + 1))
+    listofnumbers = list(range(l, r + 1))
     listofoddnumbers = []
     for num in listofnumbers:
         if num % 2 != 0:
@@ -7,4 +13,8 @@ def oddNumbers(l, r):
     return listofoddnumbers
 
 
-print(oddNumbers(1, 15))
+n = oddNumbers(100, 1500)
+
+
+for num in n:
+    print num
